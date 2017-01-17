b83a6d98ff6404fa85b2ea4ff3b2a8bbf5f4f625
Jason Sherman <jsn.sherman@gmail.com>
2017-01-17 14:21:36 -0600

    Set timeouts on all motd commands.

--- files/motd.sh
+++ files/motd.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 # Inspired by https://gist.github.com/cha55son/6042560
 
-RELEASE=`cat /etc/system-release 2>/dev/null || cat /etc/redhat-release 2>/dev/null || cat /etc/lsb-release | grep DISTRIB_DESCRIPTION | cut -d '=' -f 2 | sed -s "s/^\(\(\"\(.*\)\"\)\|\('\(.*\)'\)\)\$/\\3\\5/g" 2>/dev/null`
-USER=`whoami`
-HOSTNAME=`uname -n`
-PSA=`ps -Afl | wc -l`
+# Check for interactive shell
+if [ ! -z "${PS1}" ]; then
+
+RELEASE=`timeout 1 bash -c 'cat /etc/system-release 2>/dev/null || cat /etc/redhat-release 2>/dev/null || cat /etc/lsb-release | grep DISTRIB_DESCRIPTION | cut -d '=' -f 2 | sed -s "s/^\(\(\"\(.*\)\"\)\|\('\(.*\)'\)\)\$/\\3\\5/g" 2>/dev/null'`
+USER=`timeout 1 bash -c 'whoami'`
+HOSTNAME=`timeout 1 bash -c 'uname -n'`
+PSA=`timeout 1 bash -c 'ps -Afl | wc -l'`
 
 #System uptime
-uptime=`cat /proc/uptime | cut -f1 -d.`
+uptime=`timeout 1 bash -c 'cat /proc/uptime | cut -f1 -d.'`
 upDays=$((uptime/60/60/24))
 upHours=$((uptime/60/60%24))
 upMins=$((uptime/60%60))
 upSecs=$((uptime%60))
 
 #System load
-LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+LOAD1=`timeout 1 bash -c 'cat /proc/loadavg | awk {"print $1"}'`
+LOAD5=`timeout 1 bash -c 'cat /proc/loadavg | awk {"print $2"}'`
+LOAD15=`timeout 1 bash -c 'cat /proc/loadavg | awk {"print $3"}'`
 
 #Mem Usage
-MEM=`free -t -m`
+MEM=`timeout 1 bash -c 'free -t -m'`
 
 #Disk Usage
-DISK=`df -h | grep --invert '^none\|tmpfs\|udev'`
+DISK=`timeout 2 bash -c 'df -h | grep --invert "^none\|tmpfs\|udev"'`
 
 echo "===========================================================================
 - Hostname............: $HOSTNAME
@@ -41,3 +44,4 @@ $MEM
 $DISK
 ===========================================================================
 "
+fi
